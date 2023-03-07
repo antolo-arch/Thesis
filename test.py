202c1ec9799f02d2a1deb597c0c313f9b2c92e2f
Antoni Stroinski <55943882+antolo-arch@users.noreply.github.com>
2023-03-07 10:18:03 +0100

    add more stuff

--- test.py
+++ test.py
@@ -1,18 +1,11 @@
 from templater3 import Templater
 import re 
-# documents_to_learn = ['<b> spam and eggs </b>', '<b> ham and spam </b>',
-#                           '<b> white and black </b>'] # list of documents
-# template = Templater()
-# for document in documents_to_learn:
-#     template.learn(document)
+from wikimapper import WikiMapper
+from titlecase import titlecase
+from SPARQLWrapper import SPARQLWrapper, JSON
 
-# print('Template created:', template._template)
-
-
-# document_to_parse = '<b> yellow and blue </b>'
-# print('Parsing other document:', template.parse(document_to_parse))
-
-# print('Filling the blanks:', template.join(['', 'red', 'orange', '']))
+user_agent = "ThesisDataExtractor @github antolo-arch"
+sparql = SPARQLWrapper("https://query.wikidata.org/sparql", agent=user_agent)
 
 class WikiList:
 
@@ -20,8 +13,8 @@ class WikiList:
         file_name, word_list = tab_list.split('\t')
         self.file_name: str = file_name
         self.word_list: list = word_list.strip().split('\\n')
-        self.template: Templater = None
         self.entities_word_list: list = self.create_entity_word_list()
+        self.entities_id_list: list = self.create_entity_id_list()
 
     def create_template(self):
         template = Templater()
@@ -35,18 +28,91 @@ class WikiList:
         word_to_parse = self.word_list[0]
         print(word_to_parse, self.template._template, self.template.parse(word_to_parse))
 
-    def create_entity_word_list(self):
+    def create_entity_word_list(self) -> list:
         entity_word_list = []
         for word in self.word_list:
-            entity_word_list.append(str(re.findall(r'\[\[(.+?)\]\]',word)))
+            entity_word_list.append(re.findall(r'\[\[(.+?)\]\]',word))
         return entity_word_list
 
+    def preprocess_entity(self, entity: str) -> str:
+        if entity.find("|"):
+            entity = entity.split("|")
+            entity = entity[0]
+        entity = entity.replace('wikt:',"")
 
-# Test templater 
+        title_case_entity = titlecase(entity).replace(' ', '_')
+
+        joined = entity.replace(' ', '_')
+
+        capitalized_entity = entity.replace(' ', '_').capitalize()
+        
+        return title_case_entity, capitalized_entity, joined
+
+    def create_entity_id_list(self) -> list:
+        results = []
+        for entities in self.entities_word_list:
+            one_list = []
+            for entity in entities:
+                title_case, capitalized, joined = self.preprocess_entity(entity)
+                id = mapper.title_to_id(title_case)
+                if id is None:
+                    id = mapper.title_to_id(capitalized)
+                    if id is None:
+                        id = mapper.title_to_id(joined)
+                if id is not None:
+                    one_list.append(id)
+                else:
+                    one_list.append("")
+            results.append(one_list)
+        return results
+
+    def get_instance_of(self, entity: str) -> 'list[str]':
+        instance_of = []
+        query = '''SELECT ?classLabel WHERE {{
+        wd:{entity} wdt:P31 ?class.
+            SERVICE wikibase:label {{
+            bd:serviceParam wikibase:language "en" .
+            }}
+        }}'''.format(entity=entity)
+        sparql.setQuery(query)
+
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        for result in results['results']['bindings']:
+            instance_of.append(result['classLabel']['value'])
+        return instance_of
+
+    def create_instance_counter(self):
+        instance_counter = {}
+        for entities in self.entities_id_list:
+            for entity in entities:
+                instances = self.get_instance_of(entity)
+                for instance in instances:
+                    if instance in instance_counter:
+                        instance_counter[instance]+=1
+                    else:
+                        instance_counter[instance]=1
+        return instance_counter
+    
+    def create_replaced_entities_with_id_list(self):
+        pass
+    #replace entities with the first instance, and then use templater
+    
+
+mapper = WikiMapper("index_enwiki-latest.db")
 lines = open('simplewiki-20211120-lists-1k.tsv').readlines()
-for line in lines[100:101]:
+for line in lines[:1]:
     one_list = WikiList(line)
-    print(one_list.entities_word_list)
-    print(one_list.word_list)
-    # word_list = word_list.split('\\n')
-    # print(word_list)
+    word_list = one_list.word_list
+    entity_id_list = one_list.entities_id_list
+    entity_word_list = one_list.entities_word_list
+    for x, row in enumerate(word_list):
+        for i, word in enumerate(entity_word_list[x]):
+            if entity_id_list[x][i] == "":
+                #if id is unrecognizable, delete the row
+                pass
+            row = row.replace(f'[[{word}]]',entity_id_list[x][i])
+        print(row)
+    print(one_list.get_instance_of("Q2532"))
+            
+    
