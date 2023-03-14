1ca951e4714c113ade2b3bdfc1c56293efbf0c1e
Antoni Stroinski <55943882+antolo-arch@users.noreply.github.com>
2023-03-14 09:13:54 +0100

    test new stuff

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ class WikiList:
     def create_entity_word_list(self) -> list:
         entity_word_list = []
         for word in self.word_list:
-            entity_word_list.append(re.findall(r'\[\[(.+?)\]\]',word))
+            entity_word_list.append(re.findall(r'\[\[(.+?)\]\]',word)) 
         return entity_word_list
 
     def preprocess_entity(self, entity: str) -> str:
@@ -82,37 +82,65 @@ class WikiList:
             instance_of.append(result['classLabel']['value'])
         return instance_of
 
-    def create_instance_counter(self):
-        instance_counter = {}
+    def create_instance_list(self):
+        counter = {}
+        instances_list = []
+
         for entities in self.entities_id_list:
             for entity in entities:
                 instances = self.get_instance_of(entity)
-                for instance in instances:
-                    if instance in instance_counter:
-                        instance_counter[instance]+=1
+                if len(instances) == 1:
+                    instances_list.append(instances[0])
+                    if instances[0] in counter:
+                        counter[instances[0]]+=1
                     else:
-                        instance_counter[instance]=1
-        return instance_counter
+                        counter[instances[0]] = 1
+                elif len(instances) > 1:
+                    for instance in instances:
+                        if instance in counter:
+                            counter[instance]+=1
+                        else:
+                            counter[instance] = 1
+                    instances_list.append(instances)
+                elif len(instances) == 0:
+                    instances_list.append('None')
+        for i, instance in enumerate(instances_list):
+            if type(instance) is list:
+                most_common = max(instance, key = lambda x: counter[x])
+                instances_list[i] = most_common
+
+        return instances_list
+
+
+                
     
     def create_replaced_entities_with_id_list(self):
         pass
     #replace entities with the first instance, and then use templater
+
+    #we get instances -> we check if any of them is in a dictionary, if one of them is in dict, we select the one that exists, and remove the other ones and if none exist, then we add them all to the dict
     
 
 mapper = WikiMapper("index_enwiki-latest.db")
 lines = open('simplewiki-20211120-lists-1k.tsv').readlines()
+new_rows = []
 for line in lines[:1]:
+    instance_counter = 0
     one_list = WikiList(line)
     word_list = one_list.word_list
     entity_id_list = one_list.entities_id_list
     entity_word_list = one_list.entities_word_list
+    instance_list = one_list.create_instance_list()
+    instance_list.append('None')
     for x, row in enumerate(word_list):
         for i, word in enumerate(entity_word_list[x]):
             if entity_id_list[x][i] == "":
                 #if id is unrecognizable, delete the row
+                instance_counter+=1
                 pass
-            row = row.replace(f'[[{word}]]',entity_id_list[x][i])
-        print(row)
-    print(one_list.get_instance_of("Q2532"))
-            
+            row = row.replace(f'[[{word}]]',instance_list[instance_counter])
+            instance_counter+=1
+            new_rows.append(row)
+
+print(new_rows)
     
